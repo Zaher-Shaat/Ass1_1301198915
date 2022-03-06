806ef17783d368e403da264ed6cbc0d049a1b02c
Zaher-Shaat <amza2amza2@gmail.com>
2022-03-06 16:20:00 +0200

    first 6-3-22

---
+++ Q1.php
@@ -0,0 +1,5 @@
+<?php
+printf("Welcome To PHP
+Tomorrow I'll learn PHP global.
+");
+?>
---
+++ Q2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <?php $var = 'PHP Tutorial'; ?>
+    <title>
+        <?php echo "<h3> $var </h3>"; ?>
+    </title>
+</head>
+<body>
+    <h3>
+        <?php echo $var; ?>
+    </h3>
+    <p>
+    PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.Go to the PHP Tutorial.
+    </p>
+</body>
+</html>
